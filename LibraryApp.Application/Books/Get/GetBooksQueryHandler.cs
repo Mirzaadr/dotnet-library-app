88b5efbcf4a4a908ce0934763794c4b5db4cbdba
Mirzaadr <mirzaadr34@yahoo.com>
2025-10-26 11:28:17 +0700

    pagination to books query

--- LibraryApp.Application/Books/Get/GetBooksQueryHandler.cs
+++ LibraryApp.Application/Books/Get/GetBooksQueryHandler.cs
@@ -1,10 +1,11 @@
+using LibraryApp.Domain.Books;
 using LibraryApp.Domain.Common.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraryApp.Application.Books.Get;
 
-public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, Result<List<GetBooksResponse>>>
+public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, Result<PagedList<GetBooksResponse>>>
 {
     private readonly IAppDBContext _context;
 
@@ -13,7 +14,7 @@ public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, Result<List<G
         _context = context;
     }
 
-    public async Task<Result<List<GetBooksResponse>>> Handle(GetBooksQuery query, CancellationToken cancellationToken)
+    public async Task<Result<PagedList<GetBooksResponse>>> Handle(GetBooksQuery query, CancellationToken cancellationToken)
     {
         var bookQuery = _context.Books.AsQueryable();
 
@@ -23,7 +24,7 @@ public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, Result<List<G
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(term => term.ToLower())
                 .ToList();
-            
+
             bookQuery = bookQuery.Where(book =>
                 searchTerms.All(term => book.Title.ToLower().Contains(term)) ||
                 searchTerms.All(term => book.Author.ToLower().Contains(term)) ||
@@ -31,27 +32,28 @@ public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, Result<List<G
             );
         }
 
-        var books = await bookQuery
-          .OrderBy(b => b.CreatedAt)
-          .Skip((query.PageNumber - 1) * query.PageSize)
-          .Take(query.PageSize)
-          .Select(book => new GetBooksResponse
-          {
-              Id = book.Id.Value,
-              Title = book.Title,
-              Author = book.Author,
-              Genre = book.Genre,
-              Rating = book.Rating,
-              CoverUrl = book.CoverUrl,
-              CoverColor = book.CoverColor,
-              TotalCopies = book.TotalCopies,
-              AvailableCopies = book.AvailableCopies,
-              Summary = book.Summary,
-              CreatedAt = book.CreatedAt,
-              UpdatedAt = book.UpdatedAt
-          })
-          .ToListAsync(cancellationToken);
+        var pagedResponses = await PagedList<GetBooksResponse>.CreateAsync(
+            bookQuery.Select(book => new GetBooksResponse
+            {
+                Id = book.Id.Value,
+                Title = book.Title,
+                Author = book.Author,
+                Genre = book.Genre,
+                Rating = book.Rating,
+                CoverUrl = book.CoverUrl,
+                CoverColor = book.CoverColor,
+                TotalCopies = book.TotalCopies,
+                AvailableCopies = book.AvailableCopies,
+                Summary = book.Summary,
+                CreatedAt = book.CreatedAt,
+                UpdatedAt = book.UpdatedAt
+            }),
+            query.PageNumber,
+            query.PageSize
+        );
 
-        return books;
+        return pagedResponses;
+
+        // return books;
     }
 }
